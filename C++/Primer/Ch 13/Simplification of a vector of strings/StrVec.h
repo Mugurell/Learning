0221bbf3c3c2aa0485cf624059cc5ad7aaed492c
Mugurell <petrumugurel@gmail.com>
2015-07-10 17:45:32 +0300

    showoff

--- C++/Primer/Ch 13/Simplification of a vector of strings/StrVec.h
+++ C++/Primer/Ch 13/Simplification of a vector of strings/StrVec.h
@@ -3,7 +3,7 @@
  *
  *  Author:           Lingurar Petru-Mugurel
  *  Written:          09 Jul 2015, 21:29:38:349
- *  Last updated:           ---
+ *  Last updated:     10 Jul 2015, 17:53:26:095
  *
  *  Compilation:  g++ -std=c++14 -Wall -Werror -Wextra -pedantic -Wshadow
  *   (g++ 5.1)        -Woverloaded-virtual -Winvalid-pch -Wcast-align
@@ -16,6 +16,10 @@
  *  Description:
  *  Simplified implementation of the memory allocation strategy for a
  *  vector-like class.
+ *  Exercise 13.39: Write your own version of StrVec, including versions of
+ *  reserve, capacity, and resize.
+ *  Exercise 13.40: Add a constructor that takes an initializer_list<string> to
+ *  your StrVec class.
  *
  *  Bugs:
  *  --- None ---
@@ -31,12 +35,13 @@
 
 
 
-#include <iostream>
-
-
-
 #ifndef SIMPLIFICATION_OF_A_VECTOR_OF_STRINGS_STRVEC_H
 #define SIMPLIFICATION_OF_A_VECTOR_OF_STRINGS_STRVEC_H
+
+#include <string>
+#include <memory>       // for std::allocator
+#include <initializer_list>
+
 
 
 class StrVec {
@@ -44,16 +49,33 @@ public:
     StrVec()    // the allocator member will be default initialized
         : firstElement(nullptr), firstFree(nullptr), offTheEnd(nullptr) { }
 
+    StrVec(const std::initializer_list<std::string> &stringList);
+
     StrVec(const StrVec &constructFromMe);
     StrVec& operator=(const StrVec &copyFromMe);
     ~StrVec();
 
-// some public methods analoguous to the ones of a std::vector
+// some public methods analogous to the ones of a std::vector
+    // used to construct another element at *firstFree
     void push_back(const std::string&);
+
+    // returns the number of elements actually in use
     size_t size() const { return firstFree - firstElement; }
-    size_t capacity() const { return offTheEnd - firstFree; }
+
+    // returns the number of elements that the StrVec can hold
+    size_t capacity() const { return offTheEnd - firstElement; }
+
+    // allocate space for at least n elements - newSize.
+    void reserve(const size_t &newSize);
+
+    // resize the container so that it contains exactly the requested elements
+    void resize(const size_t &newSize);
+
+    // returns a pointer to the first constructed element
     std::string *begin() const { return firstElement; }
-    std::string *end() { return firstFree; }
+
+    // returns a pointer to one past the last constructed element
+    std::string *end() const { return firstFree; }
 
 private:
     // will ensure that there is room to add at least one element
@@ -62,7 +84,7 @@ private:
 // utilities used by the copy constructor, assignment operator, destructor
     // will allocate space and copy a given range of elements
     std::pair<std::string*, std::string*> allocAndCopy
-            (const std::string*, const std::string*);
+            (const std::string *begin, const std::string *end);
 
     // will destroy the constructed elements and deallocate the space.
     void free();
@@ -72,13 +94,12 @@ private:
 
 // member variables
     // To try and mimic std::vector's approach to allocate memory (preallocate
-    // contiguous space, etc) we’ll use an allocator to obtain raw memory.
+    // contiguous space, etc) we'll use an allocator to obtain raw memory.
     static std::allocator<std::string> alloc;
 
     std::string *firstElement; // pointer to the first element in the array
     std::string *firstFree;    // pointer to the first free element in the array
     std::string *offTheEnd;    // pointer to one past the end of the array
-
 };
 
 
--- C++/Primer/Ch 13/Simplification of a vector of strings/main.cpp
+++ C++/Primer/Ch 13/Simplification of a vector of strings/main.cpp
@@ -3,7 +3,7 @@
  *
  *  Author:           Lingurar Petru-Mugurel
  *  Written:          09 Jul 2015, 20:19:39:683
- *  Last updated:           ---
+ *  Last updated:     10 Jul 2015, 17:53:15:382
  *
  *  Compilation:  g++ -std=c++14 -Wall -Werror -Wextra -pedantic -Wshadow  
  *   (g++ 5.1)        -Woverloaded-virtual -Winvalid-pch -Wcast-align
@@ -22,10 +22,11 @@
  *  However, this strategy does not work for every class; some classes need to
  *  do their own allocation. Such classes generally must define their own
  *  copy-control members to manage the memory they allocate.
- *  As an example, we’ll implement a simplification of the library vector class.
- *  Among the simplifications we’ll make is that our class will not be a
- *  template. Instead, our class will hold strings. Thus, we’ll call our
+ *  As an example, weï¿½ll implement a simplification of the library vector class.
+ *  Among the simplifications weï¿½ll make is that our class will not be a
+ *  template. Instead, our class will hold strings. Thus, weï¿½ll call our
  *  class StrVec.
+ *
  *
  *  Bugs:
  *  --- None ---
@@ -42,4 +43,23 @@
 
 
 #include <iostream>
+
+#include "StrVec.h"
+
+
+int main()
+{
+    std::cout << "\n\nThis is just a test!\n\n";
+
+    StrVec vec{"Ana"};
+    std::cout << "vec.capacity() = " << vec.capacity() << "\n\n";
+    vec.push_back("Aneta");
+    vec.push_back("Aniela");
+    std::cout << "vec.capacity() = " << vec.capacity() << "\n\n";
+    vec.reserve(20);
+    std::cout << "After vec.reserve(20) - " << vec.capacity() << "\n\n";
+    vec.resize(10);
+    std::cout << "After vec.resize(10) - " << vec.capacity() << "\n\n";
+    return 0;
+}
 
